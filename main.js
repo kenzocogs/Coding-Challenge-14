1f3b6b7d8a73362a7b2cb4e4d6a4cea03e9ebb30
Kenzo Cogswell <kcogswell@usf.edu>
2024-11-03 20:48:13 -0500

    Dynamically display tickets on the page

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// Task2: Fetch Tickets Using Async/Await and Handle Errors
+// Task 2: Fetch Tickets Using Async/Await and Handle Errors
 
 async function fetchCustomerTickets () {
     try {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     // throwing error if theres a network problem
     if (!response.ok) {
         throw new Error (`Network response was not okay`);
@@ -12,12 +12,38 @@ async function fetchCustomerTickets () {
     if (customerTickets.length === 0) {
         throw new Error ('There are no unresolved tickets');
     }
+    // calling the display function with the fetched JSON data
+    displayTickets (customerTickets);
 }
 // catch to handle other errors
     catch (error) {
-        console.error (`ERROR: could not properly fetch data (${error.status})`);
+        console.error (`ERROR: could not properly fetch data (${error})`);
     }
 }
 
+// Task 3: Display Tickets Dynamically on the Page
+
+// function to display tickets
+function displayTickets (tickets) {
+    
+    tickets.forEach(ticket => {
+        // defining DOM elements
+        const container = document.getElementById('container')
+        const ticketElement = document.createElement('div')
+        ticketElement.classList.add('ticket');
+        // formatting ticket display
+        ticketElement.innerHTML = ` 
+        <h2>Ticket: ${ticket.id}</h2>
+        <p><i>Customer: ${ticket.userId}</i></p>
+        <p>Issue: ${ticket.title}</p>
+        <p>Details: ${ticket.body}</p>
+        `;
+       container.appendChild(ticketElement);
+    }
+    );
+}
+
+// calling fetch function at the end
+fetchCustomerTickets();
 
 
